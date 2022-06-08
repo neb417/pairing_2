904d406d1f6ef5339f373b4f05c695ad6e03b67e
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-06-08 09:45:11 -0600

    Add visits

--- friends.rb
+++ friends.rb
@@ -2,3 +2,6 @@ friends = ["Seth", "Neeley", "Sherwin", "Lucian", "Maggie"]
 puts friends[2]
 puts friends[4]
 puts friends[0]
+
+visits = ["Colorado", "Italy", "Ireland", "Hawaii", "Cali"]
+puts visits.push("Texas")
